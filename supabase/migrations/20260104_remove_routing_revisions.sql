16b9c2b99dc02acb4ca14442b9829a1ac99e8231
Debola Akeredolu <aakeredolu@apple.com>
2026-01-04 17:00:18 -0800

    removed file

--- supabase/migrations/20260104_remove_routing_revisions.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Remove routing revisions concept
--- Date: 2026-01-04
--- Description: Remove the revision column from the routings table as revisions are no longer used
-
--- Drop the comment on the revision column first
-COMMENT ON COLUMN "public"."routings"."revision" IS NULL;
-
--- Remove the revision column from the routings table
-ALTER TABLE "public"."routings" DROP COLUMN IF EXISTS "revision";
